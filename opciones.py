0a6fbc61178ecc2738fadbac554afc2a22514e15
ANDREYGSM <andrey_santa@soy.sena.edu.co>
2024-08-21 13:16:42 -0500

    Codigo Opciones

---
+++ opciones.py
@@ -0,0 +1,15 @@
+print("Escoja una de las siguientes opciones")
+print("a. Jugar")
+print("b. Calcular")
+print("c. Salir")
+
+opc = input("Digite opcion: ")
+
+if opc == 'a':
+    print("Jugar")
+else:
+    if opc == 'b':
+        print("Calcular")
+    else:
+        if opc == 'c':
+            print("Salir")    
